421e0fb0379f544b673ca756f3d0fe8815c454e3
Tony Wang <tony94042@gmail.com>
2022-10-16 15:16:00 +1100

    Completed html, completed CSS, started JS

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const timeElement = document.querySelector('.watch .time');
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const resetButton = document.getElementById('reset');
 
+let seconds = 0;
+let interval = null;
+
+//event listeners
+
+//timer function
+function timer () {
+  seconds ++;
+
+  //format our time
+  let secs = seconds % 60;
+  let min = Math.floor(seconds / 60);
+}
